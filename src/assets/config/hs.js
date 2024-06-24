8d36e06091e20fd25f15d7b0cc47a63a23a5ece7
rayan@2125 <dhananjay.s.dubey20125@gmail.com>
2024-06-24 16:06:09 +0530

    this is testing code

---
+++ src/assets/config/hs.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hs = () => {
+  return (
+    <View>
+      <Text>hs</Text>
+    </View>
+  )
+}
+
+export default hs
+
+const styles = StyleSheet.create({})
